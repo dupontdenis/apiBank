a8de8f7ac50bffd74985c0d4fdb87f6f2a1aea7f
Denis Dupont <denis.dupont@univ-evry.fr>
2022-04-17 08:32:05 +0200

    extract ctrl from router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const crypto = require('crypto');
-const db = require('../models');
+// const db = require('../models');
+const ctrlAccount = require('../controlers/account.js');
 
 
 // Configure routes
@@ -10,72 +11,19 @@ const router = express.Router();
 router
     .route('/accounts')
     // Create an account
-    .post((req, res) => {
-        // Check mandatory request parameters
-        if (!req.body.user || !req.body.currency) {
-            return res.status(400).json({ error: 'Missing parameters' });
-        }
-
-        // Check if account already exists
-        if (db[req.body.user]) {
-            return res.status(409).json({ error: 'User already exists' });
-        }
-
-        // Convert balance to number if needed
-        let balance = req.body.balance;
-        if (balance && typeof balance !== 'number') {
-            balance = parseFloat(balance);
-            if (isNaN(balance)) {
-                return res.status(400).json({ error: 'Balance must be a number' });
-            }
-        }
-
-        // Create account
-        const account = {
-            user: req.body.user,
-            currency: req.body.currency,
-            description: req.body.description || `${req.body.user}'s budget`,
-            balance: balance || 0,
-            transactions: [],
-        };
-        db[req.body.user] = account;
-        console.table(db)
-
-        return res.status(201).json(account);
-    });
+    .post(ctrlAccount.createAccount);
 
 // ------------------ACCOUNTS_USER----------------------------
 router
     .route('/accounts/:user')
 
     // Get all data for the specified account
-    .get((req, res) => {
-        const account = db[req.params.user];
-
-        // Check if account exists
-        if (!account) {
-            return res.status(404).json({ error: 'User does not exist' });
-        }
-
-        return res.json(account);
-    })
+    .get(ctrlAccount.getUserAccount)
 
     // ----------------------------------------------
 
     // Remove specified account
-    .delete((req, res) => {
-        const account = db[req.params.user];
-
-        // Check if account exists
-        if (!account) {
-            return res.status(404).json({ error: 'User does not exist' });
-        }
-
-        // Removed account
-        delete db[req.params.user];
-
-        res.sendStatus(204);
-    })
+    .delete(ctrlAccount.deleteUserAccount)
 
 // --------------------ACCOUNTS_USER_TRANSACTIONS--------------------------
 router
